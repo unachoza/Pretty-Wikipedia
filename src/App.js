33975939cbba6a8e9a1760ef953ca2ae2507490b
Arianna Choza <unachoza@gmail.com>
2019-03-14 16:43:00 -0400

    updated readme

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        The goal of this project is to make a more visiaully appealing wikipedia search and view using React.js and the wikipedia API 
       </div>
     );
   }
