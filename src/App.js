fc40292893dba75796d10d522a61f76e4f775732
Arianna Choza <unachoza@gmail.com>
2019-03-14 16:41:03 -0400

    react app with intention

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        The goal of this project is to make a more visiaully appealing wikipedia search and view using React.js and the wikipedia API 
+      </div>
+    );
+  }
+}
+
+export default App;
