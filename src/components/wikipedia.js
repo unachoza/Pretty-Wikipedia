e87039af35f042103715ac31e857e5d8a52bd1ce
Arianna Choza <unachoza@gmail.com>
2019-04-03 22:13:15 -0400

    hitting api correctly

--- src/components/wikipedia.js
+++ src/components/wikipedia.js
@@ -3,9 +3,9 @@
   const  wikipedia = {
 
         searchWikipedia:  (query) => {
-            return fetch(`https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&list=search&srsearch=${query}&utf8=&format=json`)
-            .then(response => response.text())
-            .then(entry => ({entries: entry}))
+            return fetch(`https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${query}&format=json`)
+            .then(response => response.json())
+            .then(entry => ({entries: entry.query.searchinfo.totalhits}))
         }
     }
     
